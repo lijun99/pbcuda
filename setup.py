24d01909e3c45d176835c6c9dcf243b726b2d981
Lijun Zhu <ljzhu@gps.caltech.edu>
2025-01-06 22:30:05 -0800

    fix install path

--- setup.py
+++ setup.py
@@ -5,17 +5,15 @@ from setuptools import find_packages
 # Set the CMake arguments
 cmake_args = [
     f"-DPYTHON_EXECUTABLE={os.sys.executable}",
-    "-DCMAKE_BUILD_TYPE=Release"  # You can adjust this as needed
+    "-DCMAKE_BUILD_TYPE=Release"  # Adjust as needed for debug or release
 ]
 
 setup(
     name="pbcuda",
     version="0.1.1",
     description="A Python Interface to CUDA with pybind11",
-    author="",
     license="MIT",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    python_requires=">=3.6",
+    packages=find_packages(),  # Specify where to find the Python packages
+    include_package_data=True,  # Include other files like .txt, .md, or .dat
+    zip_safe=False,  # This is typically set to False for binary distributions
 )
